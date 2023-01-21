8c1f598cf29064306450cd26ad294b5c6d8a43d9
Luke Olson <lskywalker4422@gmail.com>
2023-01-21 16:10:16 -0500

    initial file commit

---
+++ lua/setup/packer.lua
@@ -0,0 +1,22 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+	use 'wbthomason/packer.nvim'
+	
+	use {
+		'EdenEast/nightfox.nvim',
+		config = function()
+			vim.cmd('colorscheme nightfox')
+		end
+	}
+
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.0',
+		-- or                            , branch = '0.1.x',
+		requires = { {'nvim-lua/plenary.nvim'} }
+  	}
+
+
+	
+end)
+
---
+++ lua/setup/remap.lua
@@ -0,0 +1,2 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>out", vim.cmd.Ex)

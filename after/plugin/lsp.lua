ce5e7d0ea30c97f317e845615af231970f28f2c6
Luke Olson <lskywalker4422@gmail.com>
2023-01-22 14:16:44 -0500

    added LSP for lua, rust, c

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,31 @@
+-- Learn the keybindings, see :help lsp-zero-keybindings
+-- Learn to configure LSP servers, see :help lsp-zero-api-showcase
+local lsp = require('lsp-zero')
+lsp.preset('recommended')
+
+-- (Optional) Configure lua language server for neovim
+lsp.nvim_workspace()
+
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_mappings = lsp.defaults.cmp_mappings({
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+})
+
+lsp.set_preferences({
+    sign_icons = false,
+})
+
+
+lsp.ensure_installed({
+    'sumneko_lua',
+    'rust_analyzer',
+    'clangd',
+})
+
+vim.opt.signcolumn = 'no'
+
+-- keymaps here ???
+
+lsp.setup()
--- lua/setup/sets.lua
+++ lua/setup/sets.lua
@@ -4,3 +4,6 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true

9580b9114f068d490de44b4e0632eca21d4c8a7e
Luke Olson <lskywalker4422@gmail.com>
2023-09-17 11:52:07 -0400

    switched to lazy.nvim

--- lua/setup/lazy.lua
+++ lua/setup/lazy.lua
@@ -1,37 +1,45 @@
-vim.cmd [[packadd packer.nvim]]
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-return require('packer').startup(function(use)
-	use 'wbthomason/packer.nvim'
-
-	use {
+require("lazy").setup({
+	{
 		'sainnhe/sonokai',
 		config = function()
             vim.cmd('let g:sonokai_enable_italic = 0')
             vim.cmd('let g:sonokai_disable_italic_comment = 1')
 			vim.cmd('colorscheme sonokai')
 		end
-	}
+	},
 
-	use {
+	{
 		'nvim-telescope/telescope.nvim', tag = '0.1.0',
 		-- or                            , branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
-  	}
+		dependencies = { {'nvim-lua/plenary.nvim'} }
+  	},
 
-    use {
+    {
         'nvim-lualine/lualine.nvim',
-        requires = {'kyazdani42/nvim-web-devicons', opt = true}
-    }
+        dependencies = {'kyazdani42/nvim-web-devicons', opt = true}
+    },
 
-    use {
+    {
         'nvim-treesitter/nvim-treesitter',
-        {run = ':TSUpdate'}
-    }
+    },
 
-    use {
+    {
         'VonHeikemen/lsp-zero.nvim',
-        branch = 'v1.x',
-        requires = {
+        branch = 'v2.x',
+        dependencies = {
             -- LSP Support
             {'neovim/nvim-lspconfig'},             -- Requirrd
             {'williamboman/mason.nvim'},           -- Optional
@@ -49,9 +57,7 @@ return require('packer').startup(function(use)
             {'L3MON4D3/LuaSnip'},             -- Required
             {'rafamadriz/friendly-snippets'}, -- Optional
         }
-    }
+    },
 
-    use('theprimeagen/harpoon')
-
-
-end)
+    {'theprimeagen/harpoon'},
+})

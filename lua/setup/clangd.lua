d07897f49f782699232c72f3f1fdf9db3a8a13de
Luke Olson <luke.olson@magna.com>
2023-11-30 08:43:10 -0500

    updated clangd

--- lua/setup/clangd.lua
+++ lua/setup/clangd.lua
@@ -1,7 +1,6 @@
 require('lspconfig').clangd.setup {
         cmd = {
             "clangd",
-            "--suggest-missing-includes",
             "--header-insertion=never",
         },
         filetypes = {"c", "cpp", "objc", "objcpp"},

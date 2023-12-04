8dabf627e9b5e36d034d7322b015e89656b54dec
Luke Olson <luke.olson@magna.com>
2023-12-04 16:04:24 -0500

    finally fixed that terminal close thingy

--- lua/setup/autocmds.lua
+++ lua/setup/autocmds.lua
@@ -13,9 +13,7 @@ autocmd('TermOpen', {
     callback = function()
         vim.api.nvim_feedkeys('G', 'n', false)
         vim.keymap.set('n', 'q', function()
---             local jobID = vim.fn.jobpid(vim.o.channel) -- we fix this later
---             vim.api.nvim_chan_send(jobID, "exit\r\n")
-            vim.api.nvim_win_close(0, true)
+            vim.cmd("bd!")
         end, {buffer = true})
     end
 })

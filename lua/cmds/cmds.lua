05b4bfdfe4257af9eda07a91b0674d3fdd455dfa
Luke Olson <luke.olson@magna.com>
2023-12-05 15:49:35 -0500

    fixed moving cursor when cleaning unused buffers

--- lua/cmds/cmds.lua
+++ lua/cmds/cmds.lua
@@ -2,8 +2,10 @@ local new_command = vim.api.nvim_create_user_command
 
 new_command("Clean",
     function()
+        local r,c = unpack(vim.api.nvim_win_get_cursor(0))
         print("Clearing background buffers...")
         vim.cmd("%bd|e#|bd#")
+        vim.api.nvim_win_set_cursor(0, {r,c}) -- now we go back to where we were before
     end,
     {
         nargs = 0

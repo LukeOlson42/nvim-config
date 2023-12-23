41e7ceaae59243948f3c9dc611a93ea55769e693
Luke Olson <lskywalker4422@gmail.com>
2023-12-23 12:04:36 -0500

    updated lsp config

--- lua/plugins/clangd.lua
+++
@@ -1,7 +0,0 @@
-require('lspconfig').clangd.setup {
-        cmd = {
-            "clangd",
-            "--header-insertion=never",
-        },
-        filetypes = {"c", "cpp", "objc", "objcpp"},
-}
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,7 +1,6 @@
 -- add new plugins here when added
 require('plugins/lsp')
 require('plugins/treesitter')
-require('plugins/clangd')
 require('plugins/harpoon')
 require('plugins/lualine')
 require('plugins/telescope')

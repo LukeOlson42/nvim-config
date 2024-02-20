962a4c290bfef9af637a7ac8edad38cf4f053da3
Luke Olson <lskywalker4422@gmail.com>
2024-02-20 10:44:55 -0500

    somehow I forgot about ghost textgit add -A

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -55,6 +55,9 @@ cmp.setup({
         completion = cmp.config.window.bordered(),
         documentation = cmp.config.window.bordered(),
     },
+    experimental = {
+        ghost_text = true, -- i love ghost text
+    },
 })
 
 vim.diagnostic.config({
